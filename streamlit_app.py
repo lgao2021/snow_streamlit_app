bcec9966ac4db3fe31c43809e83c02c203de65cb
lgao2021 <91770509+lgao2021@users.noreply.github.com>
2023-07-04 14:49:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
 streamlit.header('Build your own Fruit Smoothie $3.60')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # let's add a pick lit for the selection
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
