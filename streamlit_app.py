1917926c31ea146e381a64ebef253a237290f1cf
lgao2021 <91770509+lgao2021@users.noreply.github.com>
2023-07-04 14:41:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,20 @@
 import streamlit
 streamlit.title("My stream lit app")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥑🍞Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑🍞Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.header('Fruit Slush $3.60')
+streamlit.text('🥣 🥗Grean Apple')
+streamlit.text('🥣 🥗Honeydew')
+streamlit.text('🥣 🥗Lemoonade')
+streamlit.text('🥣 🥗Luchee')
+streamlit.text('🥣 🥗Passion Fruit')
+streamlit.text('🥣 🥗Thai tea')
+streamlit.header('Coffee Blast Frappes $4.45')
+streamlit.text('🍌🥭 Cappuccino')
+streamlit.text('🍌🥭 Java chip')
+streamlit.text('🍌🥭 Mocha latte')
+
+
+
