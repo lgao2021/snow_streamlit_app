36c143aa192478976874ca9e46acbfb705e98631
lgao2021 <91770509+lgao2021@users.noreply.github.com>
2023-07-04 22:29:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruid load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_row = get_fruit_load_list()
-  streamlit.dataframe(my_data_raw)
+  streamlit.dataframe(my_data_row)
 
   # streamlit.stop()
 
