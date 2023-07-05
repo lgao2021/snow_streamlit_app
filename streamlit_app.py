7af062a5a6126c88274c2bc27ffc8a6ed33d04e5
lgao2021 <91770509+lgao2021@users.noreply.github.com>
2023-07-04 22:44:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,24 +49,23 @@ def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
       my_cur.execute("select * from fruit_load_list")
       return my_cur.fetchall()
-
+# allow end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cur.cursor() as my_cur:
+      my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('"' + new_fruit +'"')")
+      return "Thanks for adding " + new_fruit
+      
 if streamlit.button('Get Fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_row = get_fruit_load_list()
   streamlit.dataframe(my_data_row)
 
-  # allow end user to add a fruit to the list
-  def insert_row_snowflake(new_fruit):
-    with my_cur.cursor() as my_cur:
-      my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('"' + new_fruit +'"')")
-      return "Thanks for adding " + new_fruit
-
-  #add another fruit selection box
-  add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-  if streamlit.button('Add a fruit to the list'):
-    my_cnx = snowflake.connnector.connect(**streamlit.secrets["snowflake"])
-    raw_inserted = insert_row_snowflake(add_my_fruit)
-    streamlit.text(row_inserted)
+#add another fruit selection box
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connnector.connect(**streamlit.secrets["snowflake"])
+  raw_inserted = insert_row_snowflake(add_my_fruit)
+  streamlit.text(row_inserted)
   
 
 
