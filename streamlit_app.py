64c596919ac488793f23e429b5a401913269b3df
lgao2021 <91770509+lgao2021@users.noreply.github.com>
2023-07-04 22:48:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def get_fruit_load_list():
       return my_cur.fetchall()
 # allow end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
-  with my_cur.cursor() as my_cur:
+  with my_cnx.cursor() as my_cur:
       my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('"' + new_fruit +'"')")
       return "Thanks for adding " + new_fruit
       
